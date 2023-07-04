104c93e4b1ca00c5ef7541b1dc093d05e06e11a6
it2111oo16 <it21110016@my.sliit.lk>
2023-07-04 17:46:06 +0530

    add route paths in app.js and change logo in index.html

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,9 @@
 // import logo from './logo.svg';
 import './App.css';
-import {BrowserRouter, Routes, Route} from "react-router-dom";
+import "primereact/resources/themes/lara-light-indigo/theme.css";
+import "primereact/resources/primereact.min.css";
+import "primeicons/primeicons.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from './pages/Home';
 import AddBlog from './pages/AddBlog';
 import ViewBlog from './pages/viewBlog';
@@ -11,10 +14,10 @@ function App() {
     <div className="App">
       <BrowserRouter>
         <Routes>
-          <Route path="/" element ={<Home/>}/>
-          <Route path='/addBlog' element={<AddBlog/>}/>
-          <Route path='/viewBlog/:id' element={<ViewBlog/>}/>
-          <Route path='/updateBlog/:id' element={<UpdateBlog/>}/>
+          <Route path="/" element={<Home />} />
+          <Route path='/addBlog' element={<AddBlog />} />
+          <Route path='/viewBlog/:id' element={<ViewBlog />} />
+          <Route path='/updateBlog/:id' element={<UpdateBlog />} />
         </Routes>
       </BrowserRouter>
     </div>
