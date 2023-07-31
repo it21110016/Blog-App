bde4bce44efc3b9e433e577a91e3c335bd2dd6ba
it2111oo16 <it21110016@my.sliit.lk>
2023-07-31 10:55:59 +0530

    create private route component, add authorization to app.js, add login funtion to navbar

--- frontend/src/components/NavBar.js
+++ frontend/src/components/NavBar.js
@@ -1,48 +1,68 @@
 import React, { useState } from 'react';
-import img from './img.png'
-import { Link } from 'react-router-dom'
-import styles from '../styles/navbar.module.css'
+import img from './img.png';
+import { Link } from 'react-router-dom';
+import styles from '../styles/navbar.module.css';
 
 const NavBar = () => {
 
-    const [mobile, setMobile] = useState(false);
+  const [mobile, setMobile] = useState(false);
 
-    return (
-        <header>
-            <nav className={styles.navbar}>
+  const handleLogout = () => {
+    // Clear the user's login status (e.g., remove the JWT token from localStorage)
+    localStorage.removeItem('token');
 
-                <Link to='/'>
-                    <img className={styles.logo} src={img} alt='Logo'></img>
-                </Link>
+    // Redirect the user to the login page
+    window.location.href = "/login";
+  };
 
-                <ul className={mobile ? styles.navmobile : styles.navLinks}
-                    onClick={() => setMobile(false)}>
+  return (
+    <header>
+      <nav className={styles.navbar}>
 
-                    <Link to='/' className={styles.home}>
-                        <li>Home</li>
-                    </Link>
-                    <Link to='#' className={styles.about}>
-                        <li>About</li>
-                    </Link>
-                    <Link to='#' className={styles.services}>
-                        <li>Services</li>
-                    </Link>
-                    <Link to='#' className={styles.contact}>
-                        <li>Contact</li>
-                    </Link>
-                    <Link to='#' className={styles.signup}>
-                        <li>Sign Up</li>
-                    </Link>
+        <Link to='/'>
+          <img className={styles.logo} src={img} alt='Logo'></img>
+        </Link>
 
-                </ul>
+        <ul className={mobile ? styles.navmobile : styles.navLinks} onClick={() => setMobile(false)}>
 
-                <button className={styles.mobileMenuIcon} onClick={() => setMobile(!mobile)}>
-                    {mobile ? (<i className='fas fa-times'></i>) : (<i className='fas fa-bars'></i>)}
-                </button>
+          <Link to='/' className={styles.home}>
+            <li>Home</li>
+          </Link>
+          <Link to='#' className={styles.about}>
+            <li>About</li>
+          </Link>
+          <Link to='#' className={styles.services}>
+            <li>Services</li>
+          </Link>
+          <Link to='#' className={styles.contact}>
+            <li>Contact</li>
+          </Link>
 
-            </nav>
-        </header>
-    );
+        </ul>
+
+        {/* Check if the user is logged in and show the appropriate link */}
+        {localStorage.getItem('token') ? (
+          <>
+            <p className={styles.logout} onClick={handleLogout}>Logout</p>
+          </>
+        ) : (
+          <>
+            <Link to='/signup' className={styles.signup}>
+              <p>Sign Up</p>
+            </Link>
+            <Link to='/login' className={styles.login}>
+              <p>Login</p>
+            </Link>
+          </>
+        )}
+
+        <button className={styles.mobileMenuIcon} onClick={() => setMobile(!mobile)}>
+          {mobile ? <i className='fas fa-times'></i> : <i className='fas fa-bars'></i>}
+        </button>
+
+      </nav>
+    </header>
+  );
 };
 
 export default NavBar;
